6ca7770fb928349c80df42b418138de325759f72
Kashnaladak <francisco.paulo@gmail.com>
2013-02-23 23:43:18 +0100

    squares abd hofstader

---
+++ src/main/js/hofstadter.js
@@ -0,0 +1,13 @@
+
+function hofstadter(n) {
+    var r = 1, s = 2;
+    var set = {1:1};
+
+    for(var i = 1; i < n ; i++){
+        r = r + s;
+        set[r] = r;
+        for(s++ ; set[s] ; s++);
+    }
+
+    return Object.keys(set);
+}
